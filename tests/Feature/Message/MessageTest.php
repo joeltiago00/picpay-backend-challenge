8a57448a16605f927cbe056e21f34204194e040f
alvez2g <joeltiago00745@gmail.com>
2024-03-29 08:15:48 -0300

    added fail test to message service

--- tests/Feature/Message/MessageTest.php
+++ tests/Feature/Message/MessageTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Message;
 
+use Exception;
 use PicPay\Message\Domain\Facades\Message;
 use Tests\Feature\FeatureTest;
 
@@ -10,5 +11,12 @@ class MessageTest extends FeatureTest
     public function testSuccess()
     {
         $this->assertTrue(Message::send('123', 'test message'));
+    }
+
+    public function testFailInvalidMessageService()
+    {
+        $this->expectException(Exception::class);
+
+        Message::service('sns')->send('123', 'test message');
     }
 }
